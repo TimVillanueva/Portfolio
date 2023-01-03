d14cbf18950e07919fe3d6c84cd9c9a67ab6f0cc
tim villanueva <timvillanueva3@gmail.com>
2023-01-03 09:52:35 -0600

    image style

--- portfolio/src/components/Image.jsx
+++ portfolio/src/components/Image.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import headshot from '../assets/headshot.jpeg'
 import styled from 'styled-components'
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+
 
 const Container = styled.div `
-
+.links{
+    disply:flex;
+    flex-direction:column;
+    height: 150px;
+    width: 65px;
+    position: absolute;
+    top: 130px;
+    right:300px;
+}
+.linkedin, .github{
+    font-size: 65px;
+    color:black;
+}
+.linkedin{
+    color:#0072b1;
+}
 
 `;
 
@@ -11,7 +28,12 @@ function Image(props) {
     return (
         <Container>
         <div className="image-box">
-            <img src={headshot} className="headshot"/>
+            <div className="links">
+            <a className="github" href="https://github.com/TimVillanueva" target="_blank"><AiFillGithub/></a>
+            <a className="linkedin" href="https://www.linkedin.com/in/timothy-villanueva/" target="_blank"><AiFillLinkedin/></a>
+            
+            </div>
+            <img src={headshot} className="headshot" />
         </div>
         </Container>
     );
