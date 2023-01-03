869b1b399bedd80896e2b655e6f082ccd7ec23cf
tim villanueva <timvillanueva3@gmail.com>
2023-01-03 10:10:31 -0600

    resume update

--- portfolio/src/components/Resume.jsx
+++ portfolio/src/components/Resume.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import resume from '../assets/resume.pdf';
+import {GrDocumentPdf} from 'react-icons/gr'
 
 const Container = styled.div `
 h5{
     margin:3px 0 10px 0;
+}
+.pdf{
+    font-size: 70px;
 }
 `;
 
@@ -28,6 +33,13 @@ function Resume(props) {
                     <li style={{marginBottom:5}}>Tools: NodeJS, Postico, Insomnia, GitHub</li>
                 </ul>
             </div>
+            <div>
+                <h4>Education</h4>
+                <h5>General Assembly | Software Engineering Immersive | Remote | October 2022 - December 2022</h5>
+                <h5>BA Psychology | Grinnell College | Grinnell,IA | August 2012- May 2016</h5>
+            </div>
+            
+            <a download="Tim Villanueva Resume 2023.pdf" href={resume}><GrDocumentPdf className="pdf"/></a>
         </div>
         </Container>
     );
